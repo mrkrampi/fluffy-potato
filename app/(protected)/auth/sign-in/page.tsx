8c0a038c3fff36c7c9970108bc384cee6cb0827b
krampi <romsava1999@gmail.com>
2024-05-07 21:58:32 +0300

    feat: add suspense

--- app/(protected)/auth/sign-in/page.tsx
+++ app/(protected)/auth/sign-in/page.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from 'react';
+
 import { SignInForm } from '@/components/auth/sign-in-form';
 
 const SignInPage = () => {
   return (
-    <SignInForm/>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignInForm/>
+    </Suspense>
   );
 };
 
--- app/(protected)/auth/sign-up/page.tsx
+++ app/(protected)/auth/sign-up/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
+
 import { SignUpForm } from '@/components/auth/sign-up-form';
 
 const SignUpPage = () => {
   return (
-    <SignUpForm/>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignUpForm/>
+    </Suspense>
+  );
 };
 
 export default SignUpPage;
