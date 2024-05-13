ac993cbbde639db6a4e6037322cb423331ba44b1
krampi <romsava1999@gmail.com>
2024-05-13 11:12:52 +0300

    feat: add &apos; symbol

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,13 +1,10 @@
-import { auth } from '@/auth';
 import { SmileIcon } from 'lucide-react';
 
 const AdminPage = async () => {
-  const session = await auth();
-
   return (
     <div className="h-full w-full flex flex-col gap-3 items-center justify-center">
       <SmileIcon className="w-20 h-20"/>
-      I don't know what to show here
+      I don&apos;t know what to show here
     </div>
   )
 }
