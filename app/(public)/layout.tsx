a637739a71f282382dcfd38068ccf20f8a893026
krampi <romsava1999@gmail.com>
2024-05-19 15:44:09 +0300

    feat: test hero bg

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -5,13 +5,17 @@ import { Header } from '@/components/markup/header';
 
 const PublicLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="border-b-[6px] border-primary-accent">
-      <Header/>
-      <main>
-        {children}
-      </main>
-      <Footer/>
-    </div>
+    <>
+      <img width="99999" height="99999" alt="none" className="hack"
+           src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c3ZnIHdpZHRoPSI5OTk5OXB4IiBoZWlnaHQ9Ijk5OTk5cHgiIHZpZXdCb3g9IjAgMCA5OTk5OSA5OTk5OSIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48ZyBzdHJva2U9Im5vbmUiIGZpbGw9Im5vbmUiIGZpbGwtb3BhY2l0eT0iMCI+PHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9Ijk5OTk5IiBoZWlnaHQ9Ijk5OTk5Ij48L3JlY3Q+IDwvZz4gPC9zdmc+"/>
+      <div className="border-b-[6px] border-primary-accent">
+        <Header/>
+        <main>
+          {children}
+        </main>
+        <Footer/>
+      </div>
+    </>
   );
 };
 
