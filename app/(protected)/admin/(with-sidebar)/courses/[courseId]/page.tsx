298c5d04c0dde43fcfbd82de0327b79990b9636b
krampi <romsava1999@gmail.com>
2024-06-27 15:19:44 +0300

    feat: fetch no store for course edit page

--- app/(protected)/admin/(with-sidebar)/courses/[courseId]/page.tsx
+++ app/(protected)/admin/(with-sidebar)/courses/[courseId]/page.tsx
@@ -2,6 +2,8 @@ import { getCourseById } from '@/db/course-queries';
 import { redirect } from 'next/navigation';
 import { CourseForm } from '@/app/(protected)/admin/(with-sidebar)/courses/_components/course-form';
 
+export const fetchCache = 'force-no-store';
+
 type Props = {
   params: {
     courseId: string;
